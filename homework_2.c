7c4cf74cd17232130fb5096d7ce8e092efdc6688
Greyson Weimer <raw0047@mix.wvu.edu>
2023-02-22 22:43:18 -0500

    Fixed shit. Im only using git for file transfer

--- homework_2.c
+++ homework_2.c
@@ -102,7 +102,7 @@ int main()
 			else
 				outputString[i] = inputString[i];
 		}
-		printf("Encrypted message: %s\n", outputString);
+		printf("Encrypted message: %.*s\n", (int)strlen(outputString), outputString);
 	}
 	else // Decrypt
 	{
@@ -113,7 +113,7 @@ int main()
 			else
 				outputString[i] = inputString[i];
 		}
-		printf("Decrypted message: %s\n", outputString);
+		printf("Decrypted message: %.*s\n", (int)strlen(outputString), outputString);
 	}
 	// Free input string
 	free(inputString);
